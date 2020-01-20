384125aaadf62f569548162f5929128ae27b69b7
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-01-20 20:20:25 +0000

    Run `crystal tool format`

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,10 +2,10 @@ require "spec"
 
 require "../src/envy"
 
-ENV_FILE = "./spec/_support/.env.yml"
+ENV_FILE     = "./spec/_support/.env.yml"
 ENV_DEV_FILE = "./spec/_support/.env.dev.yml"
 
-private ENV_FILE_PERM = File.info(ENV_FILE).permissions
+private ENV_FILE_PERM     = File.info(ENV_FILE).permissions
 private ENV_DEV_FILE_PERM = File.info(ENV_DEV_FILE).permissions
 
 Spec.before_each do
