798ccb1dcf02454e8c26e9b316ad698632aed4e9
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-01-20 20:33:26 +0000

    Refactor
    Get rid of the many tiny helper methods

--- src/envy.cr
+++ src/envy.cr
@@ -9,54 +9,46 @@ module Envy
   DEFAULT_FILE_PERM = 0o600
 
   def from_file(*files, perm = DEFAULT_FILE_PERM)
-    files.each do |file|
-      return from_file(file, perm) if File.readable?(file)
-    end
+    load do
+      files.each do |file|
+        return from_file(file, perm, force: false) if File.readable?(file)
+      end
 
-    raise Error.new("files (#{files.join(", ")}) not found or not readable")
+      raise Error.new("files (#{files.join(", ")}) not found or not readable")
+    end
   end
 
   def from_file!(*files, perm = DEFAULT_FILE_PERM)
-    files.each do |file|
-      return from_file!(file, perm) if File.readable?(file)
+    load do
+      files.each do |file|
+        return from_file(file, perm, force: true) if File.readable?(file)
+      end
+
+      raise Error.new("files (#{files.join(", ")}) not found or not readable")
     end
-
-    raise Error.new("files (#{files.join(", ")}) not found or not readable")
-  end
-
-  def from_file(file, perm : Int32 = DEFAULT_FILE_PERM)
-    load { from_file file, perm, force: false }
-  end
-
-  def from_file!(file, perm : Int32 = DEFAULT_FILE_PERM)
-    load { from_file file, perm, force: true }
   end
 
   private def from_file(file, perm : Int32 = DEFAULT_FILE_PERM, *, force : Bool)
     File.chmod(file, perm)
-    File.open(file, perm: perm) { |f| from_yaml(f, force: force) }
-  end
 
-  private def from_yaml(yaml, *, force : Bool)
-    load Hash(YAML::Any, YAML::Any).from_yaml(yaml), force: force
+    File.open(file, perm: perm) do |file|
+      load Hash(YAML::Any, YAML::Any).from_yaml(file), force: force
+    end
   end
 
   private def load(yaml : Hash, prev_key = "", *, force : Bool) : Nil
-    prev_key = prev_key.upcase
-
     yaml.each do |key, val|
-      key = key.to_s.upcase
+      env_key = "#{prev_key}_#{key}".upcase.lchop('_')
 
       case raw = val.raw
       when Hash
-        load raw, "#{prev_key}_#{key}".lchop('_'), force: force
+        load raw, env_key, force: force
       when Array
         raw.each_with_index do |x, i|
-          env_key = "#{prev_key}_#{key}_#{i}".lchop('_')
-          ENV[env_key] = x.to_s if force || ENV[env_key]?.nil?
+          env_key_i = "#{env_key}_#{i}".lchop('_')
+          ENV[env_key_i] = x.to_s if force || ENV[env_key_i]?.nil?
         end
       else
-        env_key = "#{prev_key}_#{key}".lchop('_')
         ENV[env_key] = val.to_s if force || ENV[env_key]?.nil?
       end
     end
